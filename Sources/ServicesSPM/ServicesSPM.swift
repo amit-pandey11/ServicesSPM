2017931c3c94f0ee8e84d19c217fa985a0591237
Amit Pandey <v.amitpandey93@gmail.com>
2025-10-06 19:15:51 +0530

    Initial commit

---
+++ Sources/ServicesSPM/ServicesSPM.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+public struct ServicesSPM {
+    static let version = "1.0.0"
+    
+    public static func getServicesSPMVersion() -> String {
+        return "SPM Package version : \(version)"
+    }
+}
