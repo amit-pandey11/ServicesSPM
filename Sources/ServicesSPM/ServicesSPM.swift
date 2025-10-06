08723b4c73c23f4201604c1b162c16c85eb40660
Amit Pandey <v.amitpandey93@gmail.com>
2025-10-06 19:48:09 +0530

    Version update

--- Sources/ServicesSPM/ServicesSPM.swift
+++ Sources/ServicesSPM/ServicesSPM.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 public struct ServicesSPM {
-    static let version = "1.0.0"
+    static let version = "1.0.1"
     
     public static func getServicesSPMVersion() -> String {
         return "SPM Package version : \(version)"
